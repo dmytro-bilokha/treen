cd0d3225fa6216050c79243f6176aedf849d6484
Dmytro Bilokha <dmytro@posteo.net>
2021-04-09 19:45:36 +0200

    Converted login and app components to use class with handlers in arrow functions

--- frontend/src/js/viewModels/login.js
+++ frontend/src/js/viewModels/login.js
@@ -5,28 +5,27 @@ define(["require", "exports", 'knockout', 'loginManager', "ojs/ojbootstrap", "oj
     class LoginModel {
       //TODO: implement spinner, block form during submit, handle errors with message
       constructor() {
-        const self = this;
-        self.loginText = ko.observable('');
-        self.loginErrors = ko.observableArray();
-        self.passwordText = ko.observable('');
-        self.passwordErrors = ko.observableArray();
-        self.loginAction = (event) => {
-          self.loginErrors.removeAll();
-          self.passwordErrors.removeAll();
+        this.loginText = ko.observable('');
+        this.loginErrors = ko.observableArray();
+        this.passwordText = ko.observable('');
+        this.passwordErrors = ko.observableArray();
+        this.loginAction = () => {
+          this.loginErrors.removeAll();
+          this.passwordErrors.removeAll();
           let nonValidForm = false;
-          if (self.loginText() === '') {
+          if (this.loginText() === '') {
             nonValidForm = true;
-            self.loginErrors.push({ summary: 'Invalid login', detail: 'Login should not be empty', severity: 'error' });
+            this.loginErrors.push({ summary: 'Invalid login', detail: 'Login should not be empty', severity: 'error' });
           }
-          if (self.passwordText() === '') {
+          if (this.passwordText() === '') {
             nonValidForm = true;
-            self.passwordErrors.push({ summary: 'Invalid password', detail: 'Password should not be empty', severity: 'error' });
+            this.passwordErrors.push({ summary: 'Invalid password', detail: 'Password should not be empty', severity: 'error' });
           }
           if (nonValidForm) {
             return true;
           }
           loginManager
-            .login(this.loginText(), self.passwordText())
+            .login(this.loginText(), this.passwordText())
             .done((data, textStatus, jqXHR) => {
               console.log('Success');
               console.log(textStatus);
